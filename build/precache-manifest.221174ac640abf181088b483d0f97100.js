f59703433e3d83fb7c00cd649bdc44493d136567
Muhammad Owais Warsi <owaiskhan148@gmail.com>
2019-12-23 01:46:45 +0500

    build

---
+++ build/precache-manifest.221174ac640abf181088b483d0f97100.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9dfaf9af0c44ca1a5b6cd130b9f2dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fde832f65e3cd09c49f9",
+    "url": "/static/css/main.68f939f3.chunk.css"
+  },
+  {
+    "revision": "abca6fcd11198bdeae43",
+    "url": "/static/js/2.9ba07202.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.9ba07202.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fde832f65e3cd09c49f9",
+    "url": "/static/js/main.ee38fe36.chunk.js"
+  },
+  {
+    "revision": "90611f5083fee1a2dac6",
+    "url": "/static/js/runtime-main.891dd513.js"
+  }
+]);
