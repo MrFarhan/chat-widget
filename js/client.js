328184ed32f052c4916447984d4c5ff2b3277c95
Muhammad Owais Warsi <owaiskhan148@gmail.com>
2019-12-23 02:28:06 +0500

    update URL

--- js/client.js
+++ js/client.js
@@ -6,7 +6,7 @@
     // iframe.src = `http://localhost:3000`;
 
     // uncommit below iframe src for production
-    iframe.src = `https://meet-widget.herokuapp.com/`;
+    iframe.src = `https://meetup-widget.herokuapp.com/`;
 
     iframe.style.display = "none";
     iframe.style.position = "absolute";
